a9bef93ce209302034df0d0250540e120d094138
Reem2810 <84314460+Reem2810@users.noreply.github.com>
2024-12-10 17:53:51 +0100

    remove comments

--- SortingAndSearchDemo.java
+++ SortingAndSearchDemo.java
@@ -14,14 +14,14 @@ public class SortingAndSearchDemo {
             int mid = left + (right - left) / 2;
 
             if (array[mid] == target) {
-                return mid; // Target found
+                return mid;
             } else if (array[mid] < target) {
-                left = mid + 1; // Search the right half
+                left = mid + 1;
             } else {
-                right = mid - 1; // Search the left half
+                right = mid - 1;
             }
         }
-        return -1; // Target not found
+        return -1; 
     }
 
     // Insertion Sort implementation
